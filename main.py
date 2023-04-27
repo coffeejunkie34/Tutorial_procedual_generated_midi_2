814e71ad6fb7fd4cf92860a7b8097a21e49323aa
coffeejunkie34 <131815374+coffeejunkie34@users.noreply.github.com>
2023-04-27 23:36:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,27 +2,46 @@ import random
 from midiutil import MIDIFile
 import Make_rythm as mr
 
-MF = MIDIFile(1)
 
-TRACK = 0
-CHANNEL = 0
-TIME = 0
-DURATION = 1
+C_MAJOR = [60, 62, 64, 65, 67, 69, 71, 72]
+C_MINOR = [60, 62, 63, 65, 67, 68, 70, 72]
+F_Minor = [65, 67, 68, 70, 72, 73, 75, 77]
+B_MAJOR = [70, 73, 75, 76, 78, 80, 82]
 
-VOLUME = 100
-TEMPO = 100
+SCALE_DICTIONARY = {"C_MAJOR": C_MAJOR, "C_MINOR": C_MINOR, "F_Minor": F_Minor, "B_Major": B_MAJOR}
 
-NOTES = [30, 32, 33]
+def make_midi(cycle=1):
 
-MF.addTempo(TRACK, TIME, TEMPO)
+    num = 1
+    for i in range(cycle):
+        mf = MIDIFile(1)
 
-rythm_list = mr.make_rythm(max_bars=30)
+        track = 0
+        channel = 0
+        time = 0
+        volume = 100
+
+        mf.addProgramChange(track, channel, time, random.randint(0, 127))
+        TEMPO = random.randint(40, 180)
 
 
-for bar in rythm_list:
-    for note in bar:
-        MF.addNote(TRACK, CHANNEL, random.choice(NOTES), TIME, note, VOLUME)
-        TIME += note
+        mf.addTempo(track, time, TEMPO)
 
-with open("output1.mid", "wb") as output:
-    MF.writeFile(output)
+        rythm_list = mr.make_rythm(max_bars=70)
+
+        for bar in rythm_list:
+            scale = random.choice(list(SCALE_DICTIONARY.values()))
+            note = random.choice(scale)
+            for step in bar:
+                if random.randint(0, 9) > 2:
+                    note = random.choice(scale)
+                mf.addNote(track, channel, note, time, step, volume)
+                time += step
+
+        with open(f"output{num}.mid", "wb") as output:
+            mf.writeFile(output)
+        num+=1
+
+
+
+make_midi(20)
